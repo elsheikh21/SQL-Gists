23b7f79a3f074385d7320e60a7d6288cc45ff636
El Sheikh <ahmedhelsheikh@gmail.com>
2018-03-31 21:30:40 +0200

    no message

---
+++ Hacker Rank/Advanced_Joins.sql
@@ -0,0 +1,6 @@
+-- 1. Projects
+-- Write a query to output the start and end dates of projects listed by the number of days it took to complete the project in ascending order.
+-- If there is more than one project that have the same number of completion days, then order by the start date of the project.
+-- If the End_Date of the tasks are consecutive, then they are part of the same project.
+-- Samantha is interested in finding the total number of different projects completed.
+
